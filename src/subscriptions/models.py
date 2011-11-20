eab506ed08f8b5b9950c1d55bbf0146853544cad
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-20 15:22:30 -0200

    added meta in subscription

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -8,3 +8,8 @@ class Subscription(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = u"Inscrição"
+        verbose_name_plural = u"Inscrições"
