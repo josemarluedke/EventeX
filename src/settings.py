74a8088c40ca57a6cba4d22a412a1be4582d697f
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-20 15:18:34 -0200

    created app subscriptions

--- src/settings.py
+++ src/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'core',
+	'subscriptions',
 )
 
 # A sample logging configuration. The only tangible logging
