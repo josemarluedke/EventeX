e87868560a6e61641ff9644e939f6971aeceef7c
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-20 15:18:59 -0200

    created model subscription

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
